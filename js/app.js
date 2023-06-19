4be59622ef57a0a87738c2c25e1912adec9fa7a6
Nilgun N. babazada <nilgunnb@code.local>
2023-06-19 16:29:08 +0400

    updated

--- js/app.js
+++ js/app.js
@@ -1,19 +1,9 @@
 let search = document.querySelector(".search");
 search.addEventListener("click",function(){
-    let div = document.createElement("div");
-    let input = document.createElement("input");
-    let icon = document.createElement("i");
-    div.setAttribute("class","input-icon");
-    input.setAttribute("type","text");
-    input.setAttribute("size","50");
-    input.setAttribute("placeholder","Sizə necə kömək edə bilərik?");
-    icon.setAttribute("class","fa-solid fa-xmark")
-    div.appendChild(input);
-    div.appendChild(icon);
-    document.querySelector(".box").appendChild(div);
+    let box = document.querySelector(".box");
+    let icon = document.querySelector(".input-icon i");
+    box.classList.toggle("d-none");
     icon.onclick = function(){
-        this.parentElement.remove();
+        box.classList.add("d-none");
     }
 })
-
-
